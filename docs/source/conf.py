d0db65dd7583b0ca70e38dba58720ad63a05b4aa
Brett Meier <brett.meier@ngennetworks.com>
2017-11-13 19:57:21 -0800

    Removed local theme resources

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,12 +86,10 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme_path = ["_themes",]
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
